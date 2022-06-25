a8f84839411c9491359b9c42ec819b71d266e9ea
gggbbbuuu <57154026+gggbbbuuu@users.noreply.github.com>
2022-06-25 18:03:41 +0300

    MicroJen 3.3.6

--- addons/plugin.video.microjen/resources/lib/plugins/default_play_video.py
+++ addons/plugin.video.microjen/resources/lib/plugins/default_play_video.py
@@ -5,7 +5,7 @@ import resolveurl
 
 addon_id = xbmcaddon.Addon().getAddonInfo('id')
 default_icon = xbmcaddon.Addon(addon_id).getAddonInfo('icon')
-
+playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
 
 class default_play_video(Plugin):
     name = "default video playback"
@@ -27,9 +27,12 @@ class default_play_video(Plugin):
         else:
             liz.setInfo("video", {"title": title, "plot": summary, "imdbnumber": imdb})
         liz.setArt({"thumb": thumbnail, "icon": thumbnail, "poster": thumbnail})
+        playlist.clear()
         if resolveurl.HostedMediaFile(link).valid_url():
             url = resolveurl.HostedMediaFile(link).resolve()
+            playlist.add(url,liz)
             return xbmc.Player().play(url,liz)
+        playlist.add(link,liz)
         return xbmc.Player().play(link,liz)
     
 def clean_title(title):
