4ccbf177ef493b88a38f71679f8a003928440ff8
gggbbbuuu <57154026+gggbbbuuu@users.noreply.github.com>
2023-01-20 22:21:41 +0200

    build service 19.3.5

--- addons/service.gkobu.updater/resources/lib/firstrun.py
+++ addons/service.gkobu.updater/resources/lib/firstrun.py
@@ -1,10 +1,11 @@
 ﻿# -*- coding: utf-8 -*-
 import xbmc, xbmcaddon, xbmcvfs, sys, os
 from resources.lib import notify, monitor
+import main
 KODIV = xbmc.getInfoLabel("System.BuildVersion")[:2]
 
 def gkobu_version_check():
-    addon = xbmcaddon.Addon()
+    addon = main.addon
     gkobu_version = addon.getSetting('shortcutsver') or addon.getSetting('updatesver')
     gkobu_kodi_version = gkobu_version.replace(gkobu_version[0:2], KODIV, 1)
     # if gkobuguisetprev == '' or gkobuguisetprev is None:
