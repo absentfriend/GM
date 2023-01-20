6b368c22065e0c627cb39fc3fceacc769ddd1e1f
gggbbbuuu <57154026+gggbbbuuu@users.noreply.github.com>
2023-01-20 22:02:22 +0200

    service 19.3.4

--- addons/service.gkobu.updater/runner.py
+++ addons/service.gkobu.updater/runner.py
@@ -2,7 +2,7 @@
 import xbmc, xbmcaddon, os, json
 import main
 from resources.lib import set_fen, set_theoath, set_tmdbhelper, set_subsgr, set_seren, set_alivegr, set_youtube, set_gui, set_stalker, monitor, addonupdatesprog, stopservices
-from contextlib import contextmanager
+# from contextlib import contextmanager
 from datetime import date, datetime, timedelta
 
 addon = main.addon
@@ -17,17 +17,20 @@ if serviceversion == '' or serviceversion is None:
 age = int(float(addon.getSetting('mininsleep')))
 servicelisttostop = []
 
-@contextmanager
-def busy_dialog():
-    xbmc.executebuiltin('ActivateWindow(busydialognocancel)')
-    try:
-        yield
-    finally:
-        xbmc.executebuiltin('Dialog.Close(busydialognocancel)')
+# @contextmanager
+# def busy_dialog():
+    # xbmc.executebuiltin('ActivateWindow(busydialognocancel)')
+    # try:
+        # yield
+    # finally:
+        # xbmc.executebuiltin('Dialog.Close(busydialognocancel)')
 
 
 
 if __name__ == '__main__':
+    if not addon.getSetting('gkobu_version_fixed') == 'true':
+        from resources.lib import firstrun
+        firstrun.gkobu_version_check()
     while xbmc.getCondVisibility("Window.isVisible(yesnodialog)") or xbmc.getCondVisibility("Window.isVisible(okdialog)"):
         if monitor.waitForAbort(3):
             sys.exit()
@@ -42,38 +45,38 @@ if __name__ == '__main__':
         # with busy_dialog():
             # set_theoath.setTheOathSettings()
         if not addon.getSetting('set_fen') == 'false':
-            with busy_dialog():
+            # with busy_dialog():
                 set_fen.setFenSettings()
         if not addon.getSetting('set_tmdbhelper') == 'false':
-            with busy_dialog():
+            # with busy_dialog():
                 set_tmdbhelper.setTMDBhSettings()
         if not addon.getSetting('set_subtitlesgr') == 'false':
-            with busy_dialog():
+            # with busy_dialog():
                 set_subsgr.setSubsGRSettings()
         if not addon.getSetting('set_seren') == 'false':
-            with busy_dialog():
+            # with busy_dialog():
                 set_seren.setSerenSettings()
         if not addon.getSetting('set_alivegr') == 'false':
-            with busy_dialog():
+            # with busy_dialog():
                 set_alivegr.setAliveGRSettings()
         if not addon.getSetting('set_youtube') == 'false':
-            with busy_dialog():
+            # with busy_dialog():
                 set_youtube.setYoutubeSettings()
         if not addon.getSetting('set_gui') == 'false':
-            with busy_dialog():
+            # with busy_dialog():
                 set_gui.setguiSettings()
-        with busy_dialog():
-            set_stalker.setpvrstalker()
-        with busy_dialog():
-            main.reporescue()
-            addon.setSetting('lasttimecheck', str(datetime.now()))
-            update_toggle = '{"jsonrpc":"2.0", "method":"Settings.GetSettingValue","params":{"setting":"general.addonupdates"}, "id":1}'
-            resp_toggle = xbmc.executeJSONRPC(update_toggle)
-            toggle = json.loads(resp_toggle)
-            if toggle['result']['value'] != 0:
-                xbmc.executeJSONRPC('{"jsonrpc":"2.0","method":"Settings.SetSettingValue","id":1,"params":{"setting":"general.addonupdates","value":0}}')
-            if monitor.waitForAbort(1):
-                sys.exit()
+        # with busy_dialog():
+        set_stalker.setpvrstalker()
+        # with busy_dialog():
+        main.reporescue()
+        addon.setSetting('lasttimecheck', str(datetime.now()))
+        update_toggle = '{"jsonrpc":"2.0", "method":"Settings.GetSettingValue","params":{"setting":"general.addonupdates"}, "id":1}'
+        resp_toggle = xbmc.executeJSONRPC(update_toggle)
+        toggle = json.loads(resp_toggle)
+        if toggle['result']['value'] != 0:
+            xbmc.executeJSONRPC('{"jsonrpc":"2.0","method":"Settings.SetSettingValue","id":1,"params":{"setting":"general.addonupdates","value":0}}')
+        if monitor.waitForAbort(1):
+            sys.exit()
         addon.setSetting('service_ver', latest_version)
         if main.addon.getSetting('addon.updates.monitor') == 'true':
             xbmc.executebuiltin('RunScript("special://home/addons/service.gkobu.updater/resources/lib/addonupdatesprog.py")')
@@ -93,5 +96,5 @@ if __name__ == '__main__':
     if len(servicelisttostop) > 0:
         if monitor.waitForAbort(10):
             sys.exit()
-        with busy_dialog():
-            stopservices.StopAllRunning(servicelisttostop)
+        # with busy_dialog():
+        stopservices.StopAllRunning(servicelisttostop)
