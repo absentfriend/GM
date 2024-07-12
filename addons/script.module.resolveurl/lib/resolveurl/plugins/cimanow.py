594201b53ec563530d8a57e1d99d7c83177edd12
gggbbbuuu <57154026+gggbbbuuu@users.noreply.github.com>
2024-07-12 20:24:19 +0300

    MicroJen 3.7.20, ResolveURL 5.1.142

--- addons/script.module.resolveurl/lib/resolveurl/plugins/cimanow.py
+++ addons/script.module.resolveurl/lib/resolveurl/plugins/cimanow.py
@@ -1,6 +1,6 @@
 """
     Plugin for ResolveURL
-    Copyright (C) 2019 gujal
+    Copyright (C) 2023 shellc0de
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -16,21 +16,30 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
+from six.moves import urllib_parse
 from resolveurl.lib import helpers
+from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
 
 
-class UQLoadResolver(ResolveGeneric):
-    name = 'UQLoad'
-    domains = ['uqload.com', 'uqload.co', 'uqload.io', 'uqload.to', 'uqload.ws']
-    pattern = r'(?://|\.)(uqload\.(?:[ict]om?|ws))/(?:embed-)?([0-9a-zA-Z]+)'
+class CimaNowResolver(ResolveGeneric):
+    name = 'CimaNow'
+    domains = ['cimanowtv.com']
+    pattern = r'(?://)((?:[^\.]*\.)?cimanowtv\.com)/e/([0-9a-zA-Z$:/.]+)'
 
     def get_media_url(self, host, media_id):
+        if '$$' in media_id:
+            media_id, referer = media_id.split('$$')
+            referer = urllib_parse.urljoin(referer, '/')
+        else:
+            referer = False
         return helpers.get_media_url(
             self.get_url(host, media_id),
-            patterns=[r'''sources:\s*\[['"](?P<url>[^'"]+)'''],
-            referer=False
+            patterns=[r'''<source\s*src="(?P<url>[^"]+).+?size="(?P<label>[^"]+)'''],
+            generic_patterns=False,
+            referer=referer,
+            ssl_verify=False,
+            verifypeer=False
         )
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, 'https://{host}/embed-{media_id}.html')
+        return self._default_get_url(host, media_id, template='https://{host}/e/{media_id}')
